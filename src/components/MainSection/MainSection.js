8a1a2f31dddcd88c518c3e5b7b0485461c56956f
l1ghbe <ee1zet25@gmail.com>
2021-06-08 21:41:37 +0300

    builded

--- src/components/MainSection/MainSection.js
+++ src/components/MainSection/MainSection.js
@@ -8,7 +8,7 @@ const MainSection = () => {
             <video src="./videos/video-2.mp4" autoPlay loop muted></video>
             <div className="main-section__info">
                 <h1>The Universe is Incredible!</h1>
-                <p>let's see what's inside</p>
+                <p>Let's see what's inside</p>
                 <div className="btns">
                     <Button btnStyle="btn-outlined" sizeBtn="btn-large">GET STARTED</Button>
                     <Button sizeBtn="btn-large">WATCH TRAILER</Button>
